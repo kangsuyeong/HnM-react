e73df173dc7280e9ba77c892f7a7c0bc8319144b
kangsuyeong <ekdh16@naver.com>
2024-03-27 18:11:47 +0900

    error

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductCard = () => {
   return (
     <div>
-      <img src="https://lp2.hm.com/hmgoepprod?set=source[/53/74/5374bcf56a222c2b04973f70286d2018ebcb3f5d.jpg],origin[dam],category[],type[LOOKBOOK],res[y],hmver[1]&call=url[file:/product/main]"/>
+      <img alt ="" src="https://lp2.hm.com/hmgoepprod?set=source[/53/74/5374bcf56a222c2b04973f70286d2018ebcb3f5d.jpg],origin[dam],category[],type[LOOKBOOK],res[y],hmver[1]&call=url[file:/product/main]"/>
       <div>안녕</div>
       <div>옷이름</div>
       <div>가격</div>
